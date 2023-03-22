c830f1e4401bb85eb0c80db45ea33eb9e18db88d
sfazli96 <sfazli96@gmail.com>
2023-03-22 13:32:04 -0700

    create and edit review route

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from app.models import Review, db, Product
-# from app.forms import ReviewForm
+from app.forms import ReviewForm
 import datetime
 from flask_login import login_required, current_user
 review_routes = Blueprint('reviews', __name__)
@@ -15,3 +15,28 @@ def allReviews():
         list.append(review.to_dict())
     return {'reviews': list}
 
+
+@review_routes.route('/', methods=['POST'])
+# @login_required
+def createReview():
+    date = datetime.datetime.now()
+    jsonData = request.get_json()
+    form = ReviewForm()
+    form['csrf_token'].data = request.cookies['csrf_token'] # makes a csrf_token in form object
+
+    if form.validate_on_submit():
+        recent_review = Review.query.filter_by(product_id=jsonData["product_id"], user_id=current_user.id).first()
+        new_review = Review(
+            review = jsonData["review"],
+            rating = jsonData["rating"],
+            product_id = jsonData["product_id"],
+            user_id = current_user.id,
+            created_at=date
+        )
+        print(new_review.to_dict())
+        db.session.add(new_review)
+        db.session.commit()
+
+        return new_review.to_dict()
+    else:
+        return {"message": "Bad information, Please try again"}, 400
