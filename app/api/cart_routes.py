1328a62981c4b77172f4bdd683a0196220f49b75
sfazli96 <sfazli96@gmail.com>
2023-03-23 15:14:16 -0700

    addItemToCart route

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.models import db, cartJoined, User, Cart
+from app.models import db, cartJoined, User, Cart, Product
 from sqlalchemy.orm import joinedload, session
 from flask_login import login_required, current_user
 
@@ -12,15 +12,15 @@ def readCart(id):
     # print('carts', carts)
     cart_result = []
     for cart in carts:
-        allProducts = []
+        all_product = []
         for product in cart.products:
             # print('product', product.to_dict())
             productObj = product.to_dict()
-        allProducts.append(productObj)
+        all_product.append(productObj)
 
     cart_object = cart.to_dict()
     # print('cart_obj', cart_object)
-    cart_object.update({"products": allProducts})
+    cart_object.update({"products": all_product})
     cart_result.append(cart_object)
 
     result = {
@@ -34,7 +34,7 @@ def readCart(id):
 def createCart():
     user = current_user
     print('user', user)
-    body_info = request.get_json()
+    request_data = request.get_json()
     new_cart = Cart(
         user_id = user.id
     )
@@ -55,27 +55,48 @@ def createCart():
 
 @cart_routes.route('<int:id>', methods=['DELETE'])
 def deleteCartItem(id):
-    body_info = request.get_json()
+    request_data = request.get_json()
     cart = Cart.query.get(id)
     print('cart', cart)
     if not cart:
         return cart.errors
 
-    for product in cart.products:
-        if product.id == body_info:
-            cart.product.remove(product)
+    cart.products = [product for product in cart.products if product.id != request_data]
+    print('list-----cart', cart.products)
+
+    # for product in cart.products:
+    #     if product.id == request_data:
+    #         cart.product.remove(product)
 
     db.session.commit()
 
-    cart_obj = cart.to_dict()
-    cart_obj['products'] = []
-    for product in cart.products:
-        if product.id == body_info:
-            continue
-        product_obj = product.to_dict()
-        print('prod_obj', product_obj)
-        cart_obj['products'].append(product_obj)
+    cart_obj = {
+        'id': cart.id,
+        'product': [product.to_dict() for product in cart.products],
+        'user_id': cart.user_id
+    }
+    # cart_obj = cart.to_dict()
+    # cart_obj['products'] = []
+    # for product in cart.products:
+    #     if product.id == request_data:
+    #         continue
+    #     product_obj = product.to_dict()
+    #     print('prod_obj', product_obj)
+    #     cart_obj['products'].append(product_obj)
 
     return cart_obj
+
+@cart_routes.route('<int:cart_id>/product/<int:product_id>', methods=['POST'])
+def addItemToCart(cart_id, product_id):
+    cart = Cart.query.get(cart_id)
+    print('cart', cart)
+    product = Product.query.get(product_id)
+    print('product', product)
+
+    cart.products.append(product)
+    db.session.commit()
+
+    return{"success": "Product added to cart"}
+
 
 
