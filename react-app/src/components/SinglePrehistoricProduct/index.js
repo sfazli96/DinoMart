19ba94e0772264109bad4a0a5ebd4c684a1e51e3
sfazli96 <sfazli96@gmail.com>
2023-04-12 00:08:41 -0700

    get/create favorites almost working, continue on bookings create/delete booking

--- react-app/src/components/SinglePrehistoricProduct/index.js
+++ react-app/src/components/SinglePrehistoricProduct/index.js
@@ -5,19 +5,41 @@ import { useHistory, useParams } from "react-router-dom";
 import './prehistoricProduct.css'
 import Reviews from "../Review";
 import { thunkAddToCart } from "../../store/cart";
+import { loadFavorites, readFavorites, removeFavorites } from "../../store/favorite";
+import { useState } from "react";
+import { addFavorites } from "../../store/favorite";
 
 function SinglePrehistoricProduct() {
     const dispatch = useDispatch()
     const id = useParams()
     // console.log('ID', id)
+    // const ID = parseInt(id.id)
     const history = useHistory()
     const productDetail = useSelector(state => state.products.singleProduct)
     // console.log("prod", productDetail)
     const user = useSelector(state => state.session.user)
+    const userId = user?.id
+    const favoriteDetail = useSelector(state => state.favoriteReducer?.favorites || [] || {});
+    console.log(favoriteDetail, 'FAVORITE----')
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [errors, setErrors] = useState([])
 
     useEffect(() => {
         dispatch(loadOnePrehistoricProduct(id.id))
-    }, [dispatch])
+        if (user) {
+            dispatch(readFavorites(user?.id))
+        }
+    }, [dispatch, id.id, user])
+
+    useEffect(() => {
+        if (favoriteDetail && Array.isArray(favoriteDetail) && favoriteDetail.some(favorite => favorite.id === productDetail.id)) {
+            setIsFavorite(true);
+        } else {
+          setIsFavorite(false);
+        }
+      }, [favoriteDetail, productDetail.id]);
+
+
     if (Object.values(productDetail).length === 0) {
         return (
             <div>
@@ -39,6 +61,48 @@ function SinglePrehistoricProduct() {
     //     return image
     // }
     // console.log('MULTI', multiImage())
+    // const handleClick = () => {
+    //     setErrors([])
+    //     const productId = productDetail.id
+    //     if (isFavorite) {
+    //         dispatch(removeFavorites(productId))
+    //         .then(() => {
+    //             dispatch(readFavorites(parseInt(userId)))
+    //         })
+    //         .catch(async (res) => {
+    //             const data = await res.json()
+    //             if (data && data.errors) setErrors(data.errors)
+    //         })
+    //     } else {
+    //         return dispatch(addFavorites({userId, productId}))
+    //         .then(() => {
+    //             dispatch(readFavorites(parseInt(userId)))
+    //         })
+    //         .catch(async (res) => {
+    //             const data = await res.json()
+    //             if (data && data.errors) setErrors(data.errors)
+    //         })
+    //     }
+
+    // }
+    const handleClick = () => {
+        if (user && productDetail) {
+          if (isFavorite) {
+            dispatch(removeFavorites(userId, productDetail.id))
+              .then(() => setIsFavorite(false));
+          } else {
+            dispatch(addFavorites(userId, productDetail.id))
+              .then(() => setIsFavorite(true));
+          }
+        }
+      };
+
+    // const handleDeleteFavorite = (id) => {
+    //     dispatch(removeFavorites(id)).then(() => {
+    //         dispatch(readFavorites(id))
+    //     })
+    // }
+
     const cartButton = () => {
         dispatch(thunkAddToCart(user?.id, id.id))
             .then(() => {
@@ -60,7 +124,11 @@ function SinglePrehistoricProduct() {
             {user ? (
                 <div className="add-to-cart-button">
                     <button className="cart-button" onClick={() => cartButton(id)}>Add to Cart</button>
+                    <button className="favorite-button" onClick={handleClick}>
+                        {isFavorite ? <i className="fas fa-heart"></i> : <i className="far fa-heart"></i>}
+                    </button>
                 </div>
+
             ) : null}
             <div className="review-component">
                 <Reviews />
--- react-app/src/store/favorite.js
+++ react-app/src/store/favorite.js
@@ -25,58 +25,72 @@ export const readFavorites = (userId) => async (dispatch) => {
     dispatch(loadFavorites(favorites))
 }
 
-export const addFavorites = (favorite) => async (dispatch) => {
+export const addFavorites = (userId, productId) => async (dispatch) => {
     const response = await fetch('/api/favorites', {
         method: 'POST',
         headers: {
-            "Content-Type": "application/json"
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(favorite)
-    })
+        body: JSON.stringify({
+            user_id: userId,
+            product_id: productId
+        }),
+      });
     if (response.ok) {
         const favorite = await response.json()
+        console.log('add-favorite----', favorite)
         dispatch(createFavorites(favorite))
-        console.log('add-favoriate----', favorite)
         return favorite
     }
 }
 
+export const removeFavorites = (id) => async (dispatch) => {
+    const response = await fetch(`/api/favorites/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    if (response.ok) {
+        const data = await response.json()
+        console.log('DATA-----',data)
+        dispatch(deleteFavorites(data))
+        return data
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const initialState = { }
+const initialState = {
+    favorites: []
+}
 
 export const favoriteReducer = (state = initialState, action) => {
     let newState;
     switch (action.type) {
+        case LOAD_FAVORITES:
+            newState = { ...state }
+            let allFavoritesCopy = {}
+            if (action.payload.Favorites) {
+                action.payload.Favorites.forEach(fav => {
+                    allFavoritesCopy[fav.id] = fav
+                });
+            }
+            newState.favorites = allFavoritesCopy
+            return newState
+        case ADD_FAVORITE:
+            newState = { ...state };
+            if (action.payload.Favorites) {
+                action.payload.Favorites.forEach((element) => {
+                    newState.favorites[element.id] = element;
+                });
+            }
+            return newState;
+        case REMOVE_FAVORITE:
+            newState = { ...state };
+            if (newState.favorites[action.payload.id]) {
+                delete newState.favorites[action.payload.id];
+            }
+            return newState;
+
         default:
             newState = {...state}
             return newState
