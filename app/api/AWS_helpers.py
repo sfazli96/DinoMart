96240034c35996fc573c60dc70281b94adc31546
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2023-09-29 18:19:24 -0700

    optimizing code from pylint tests

--- app/api/AWS_helpers.py
+++ app/api/AWS_helpers.py
@@ -1,7 +1,76 @@
-import boto3
-import botocore
+"""
+Amazon S3 Utility Module
+
+This module provides functions to interact with Amazon S3 
+for uploading, removing, and managing files.
+
+Module-level Constants:
+    BUCKET_NAME (str): The name of the Amazon S3 bucket to be used for file storage.
+    S3_LOCATION (str): The base URL for accessing files stored in the S3 bucket.
+    ALLOWED_EXTENSIONS (set): A set of allowed file extensions for uploaded files.
+
+Module-level Variables:
+    s3 (boto3.client): An initialized Amazon S3 client for performing S3 operations.
+
+Functions:
+    1. get_unique_filename(filename)
+        Generate a unique filename by appending a random UUID to the given filename's extension.
+        
+        Parameters:
+        - filename (str): The original filename.
+        
+        Returns:
+        - str: A unique filename in the format 'random_uuid.extension'.
+
+    2. upload_file_to_s3(file, acl="public-read")
+        Uploads a file to Amazon S3.
+        
+        Parameters:
+        - file (File-like object): The file to be uploaded.
+        - acl (str, optional): The access control list (ACL) for the uploaded file.
+            Defaults to "public-read," which makes the file publicly accessible.
+        
+        Returns:
+        - dict: A dictionary containing either a "url" key with the S3 file URL if
+            the upload is successful, or an "errors" key with a string representation
+            of the error message if an error occurs.
+
+    3. remove_file_from_s3(image_url)
+        Removes a file from Amazon S3.
+        
+        Parameters:
+        - image_url (str): The URL of the file to be removed.
+        
+        Returns:
+        - Union[bool, dict]: Returns `True` if the file was successfully deleted.
+            If an error occurs during deletion, it returns a dictionary with an "errors" key
+            containing a string representation of the error message.
+
+Examples:
+    # Upload a file to S3 with public-read ACL
+    result = upload_file_to_s3(my_file_object)
+    if "url" in result:
+        print(f"File uploaded successfully. URL: {result['url']}")
+    elif "errors" in result:
+        print(f"Error uploading file: {result['errors']}")
+    else:
+        print("Unexpected result.")
+    
+    # Remove a file from S3
+    result = remove_file_from_s3("https://s3.amazonaws.com/my-bucket/my-file.jpg")
+    if isinstance(result, bool) and result is True:
+        print("File deleted successfully.")
+    elif isinstance(result, dict) and "errors" in result:
+        print(f"Error deleting file: {result['errors']}")
+    else:
+        print("Unexpected result.")
+"""
+
 import os
 import uuid
+import boto3
+import botocore
+from botocore.exceptions import ClientError
 
 BUCKET_NAME = os.environ.get("S3_BUCKET")
 S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
@@ -15,12 +84,48 @@ s3 = boto3.client(
 
 
 def get_unique_filename(filename):
+    """
+    Generate a unique filename by appending a random UUID to the given filename's extension.
+
+    Parameters:
+    filename (str): The original filename.
+
+    Returns:
+    str: A unique filename in the format 'random_uuid.extension'.
+    """
     ext = filename.rsplit(".", 1)[1].lower()
     unique_filename = uuid.uuid4().hex
     return f"{unique_filename}.{ext}"
 
 
 def upload_file_to_s3(file, acl="public-read"):
+    """
+    Uploads a file to Amazon S3.
+
+    Args:
+        file (File-like object): The file to be uploaded.
+        acl (str, optional): The access control list (ACL) for the uploaded file.
+            Defaults to "public-read," which makes the file publicly accessible.
+
+    Returns:
+        dict: A dictionary containing either a "url" key with the S3 file URL if
+        the upload is successful, or an "errors" key with a string representation
+        of the error message if an error occurs.
+
+    Note:
+        This function assumes that you have initialized the `s3` client and defined the
+        `BUCKET_NAME` and `S3_LOCATION` variables elsewhere in your code.
+
+    Example:
+        # Upload a file to S3 with public-read ACL
+        result = upload_file_to_s3(my_file_object)
+        if "url" in result:
+            print(f"File uploaded successfully. URL: {result['url']}")
+        elif "errors" in result:
+            print(f"Error uploading file: {result['errors']}")
+        else:
+            print("Unexpected result.")
+    """
     try:
         s3.upload_fileobj(
             file,
@@ -31,23 +136,44 @@ def upload_file_to_s3(file, acl="public-read"):
                 "ContentType": file.content_type
             }
         )
-    except Exception as e:
-        # in case the our s3 upload fails
-        return {"errors": str(e)}
+    except ClientError as error:
+        # In case the S3 upload fails
+        return {"errors": str(error)}
 
     return {"url": f"{S3_LOCATION}{file.filename}"}
 
 
 def remove_file_from_s3(image_url):
-    # AWS needs the image file name, not the URL,
-    # so we split that out of the URL
+    """
+    Removes a file from Amazon S3.
+
+    Args:
+        image_url (str): The URL of the file to be removed.
+
+    Returns:
+        Union[bool, dict]: Returns `True` if the file was successfully deleted.
+        If an error occurs during deletion, it returns a dictionary with an "errors" key
+        containing a string representation of the error message.
+
+    Note:
+        This function assumes that you have initialized the `s3` client and defined the
+        `BUCKET_NAME` variable elsewhere in your code.
+
+    Example:
+        result = remove_file_from_s3("https://s3.amazonaws.com/my-bucket/my-file.jpg")
+        if isinstance(result, bool) and result is True:
+            print("File deleted successfully.")
+        elif isinstance(result, dict) and "errors" in result:
+            print(f"Error deleting file: {result['errors']}")
+        else:
+            print("Unexpected result.")
+    """
     key = image_url.rsplit("/", 1)[1]
-    print(key)
     try:
         s3.delete_object(
-        Bucket=BUCKET_NAME,
-        Key=key
+            Bucket=BUCKET_NAME,
+            Key=key
         )
-    except Exception as e:
-        return { "errors": str(e) }
+    except ClientError as error:
+        return {"errors": str(error)}
     return True
--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,8 +1,36 @@
+"""
+Authentication Blueprint
+
+This module defines routes and functions for user authentication 
+using Flask and Flask-Login.
+
+Routes:
+- `/`: GET request for authenticating a user. Returns the user's 
+information if authenticated.
+- `/login`: POST request for logging a user in.
+- `/logout`: GET request for logging a user out.
+- `/signup`: POST request for creating a new user and logging them in.
+- `/unauthorized`: GET request to handle unauthorized access.
+
+Functions:
+- `validation_errors_to_error_messages(validation_errors)`: Converts WTForms 
+validation errors into a list of error messages.
+
+Please note that this module relies on the following imported modules and objects:
+- `Flask` from `flask`
+- `Blueprint`, `jsonify`, `session`, `request` from `flask`
+- `User`, `db` from `app.models`
+- `LoginForm`, `SignUpForm` from `app.forms`
+- `current_user`, `login_user`, `logout_user`, `login_required` from `flask_login`
+
+This module is part of the larger Flask application for user authentication.
+"""
+
 from flask import Blueprint, jsonify, session, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
-from flask_login import current_user, login_user, logout_user, login_required
 
 auth_routes = Blueprint('auth', __name__)
 
